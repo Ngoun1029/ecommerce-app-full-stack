c1427cb4ab6359f4be9c6bdb318f575acd751163
PC <nguonmarady12@gmail.com>
2026-02-22 21:43:01 +0700

    register fix remove image

--- src/app/api/auths/registers/route.ts
+++ src/app/api/auths/registers/route.ts
@@ -7,25 +7,26 @@ import bcrypt from "bcryptjs";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
-    const formData = await request.formData();
-    const name = (formData.get("name") as string)?.trim();
-    const email = (formData.get("email") as string)?.trimEnd();
-    const password = formData.get("password") as string;
-    const dob = formData.get("dob") as string;
-    const imageFile = formData.get("image") as File;
+    const body : RegisterRequest = await request.json()
+    // const formData = await request.formData();
+    // const name = (formData.get("name") as string)?.trim();
+    // const email = (formData.get("email") as string)?.trimEnd();
+    // const password = formData.get("password") as string;
+    // const dob = formData.get("dob") as string;
+    // const imageFile = formData.get("image") as File;
 
-    if (!name) {
-      return NextResponse.json(
-        { status: "error", message: "Name is required" },
-        { status: 400 },
-      );
-    }
-    if (!imageFile) {
-      return NextResponse.json(
-        { status: "error", message: "Image is required" },
-        { status: 400 },
-      );
-    }
+    // if (!name) {
+    //   return NextResponse.json(
+    //     { status: "error", message: "Name is required" },
+    //     { status: 400 },
+    //   );
+    // }
+    // if (!imageFile) {
+    //   return NextResponse.json(
+    //     { status: "error", message: "Image is required" },
+    //     { status: 400 },
+    //   );
+    // }
     const roleAsUser = await findOneWithRelations(
       "roles",
       "user",
@@ -41,23 +42,23 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       });
     }
 
-    const imageUrl = await uploadToR2(
-      {
-        buffer: Buffer.from(await imageFile.arrayBuffer()),
-        originalName: imageFile.name,
-        mimeType: imageFile.type,
-      },
-      "users",
-    );
+    // const imageUrl = await uploadToR2(
+    //   {
+    //     buffer: Buffer.from(await imageFile.arrayBuffer()),
+    //     originalName: imageFile.name,
+    //     mimeType: imageFile.type,
+    //   },
+    //   "users",
+    // );
 
     await prisma.users.create({
       data: {
-        name,
-        email,
-        password: await bcrypt.hash(password, 10),
-        image: imageUrl,
+        name: body.name,
+        email: body.email,
+        password: await bcrypt.hash(body.password, 10),
+        image: null,
         roleId: roleAsUser.id,
-        dob,
+        dob: body.dob,
       },
     });
 
--- utils/file.ts
+++ utils/file.ts
@@ -6,35 +6,34 @@ type UploadFileInput = {
   buffer: Buffer;
   originalName: string;
   mimeType?: string;
-};
+} | null;
 
 export async function uploadToR2(
-  file: UploadFileInput | null,
+  file: UploadFileInput,
   directory: string,
-  existingKey?: string,
+  existingKey?: string
 ): Promise<string | null> {
   if (!file) return null;
 
   const extension = file.originalName.split(".").pop();
   const key = `${directory}/${uuidv4()}.${extension}`;
+
   try {
-    // Upload file
     await r2.send(
       new PutObjectCommand({
         Bucket: process.env.R2_BUCKET_NAME!,
         Key: key,
         Body: file.buffer,
         ContentType: file.mimeType,
-      }),
+      })
     );
 
-    // Delete old file if exists
     if (existingKey) {
       await r2.send(
         new DeleteObjectCommand({
           Bucket: process.env.R2_BUCKET_NAME!,
           Key: existingKey,
-        }),
+        })
       );
     }
 
